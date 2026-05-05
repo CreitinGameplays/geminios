5e51df680dd7d08a4fb41b890c572e26a7cfae1d
Creitin Gameplays <clton999@outlook.com>
2026-05-05 17:39:29 -0300

    fixes.

--- ports/adwaita-icon-theme/build.sh
+++ ports/adwaita-icon-theme/build.sh
@@ -3,5 +3,8 @@ set -e
 ADWAITA_ICON_THEME_VER="42.0"
 download_and_extract "https://download.gnome.org/sources/adwaita-icon-theme/42/adwaita-icon-theme-$ADWAITA_ICON_THEME_VER.tar.xz" "adwaita-icon-theme-$ADWAITA_ICON_THEME_VER.tar.xz" "adwaita-icon-theme-$ADWAITA_ICON_THEME_VER"
 cd "$DEP_DIR/adwaita-icon-theme-$ADWAITA_ICON_THEME_VER"
-./configure --prefix=/usr --host=x86_64-linux-gnu
+# The host may provide gtk-encode-symbolic-svg, but this build environment
+# does not stage the SVG pixbuf loader that tool depends on. Disable the
+# optional symbolic PNG conversion path and install the scalable icons only.
+ac_cv_path_GTK_ENCODE_SYMBOLIC_SVG=false ./configure --prefix=/usr --host=x86_64-linux-gnu
 make install DESTDIR="$ROOTFS"
