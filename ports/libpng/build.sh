0e178cb7c0c5ef1a3bf7d99c21b8cb8ca735e6e7
Creitin Gameplays <clton999@outlook.com>
2026-05-06 14:47:14 -0300

    chore: clean up previous build artifacts before configuring libpng

--- ports/libpng/build.sh
+++ ports/libpng/build.sh
@@ -5,6 +5,10 @@ LIBPNG_VER="1.6.40"
 download_and_extract "https://downloads.sourceforge.net/libpng/libpng-$LIBPNG_VER.tar.xz" "libpng-$LIBPNG_VER.tar.xz" "libpng-$LIBPNG_VER"
 
 cd "$DEP_DIR/libpng-$LIBPNG_VER"
+if [ -f Makefile ]; then
+    make distclean >/dev/null 2>&1 || true
+fi
+rm -f config.cache config.status pnglibconf.h pnglibconf.c pnglibconf.out pnglibconf.pre pnglibconf.dfn
 ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu --disable-static --host=x86_64-linux-gnu
 make -j$JOBS
 make install DESTDIR="$ROOTFS"
